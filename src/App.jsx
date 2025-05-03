04c2e848357cf6d3a483af72cdb8d8596afb9f2b
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-05-04 01:44:31 +0900

    Category.jsx 구현

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,8 @@ function App() {
     // 모두 구현 후 구현 완료한 페이지 캡쳐하여 pr에 완성본 항목에 사진 첨부 부탁드립니다.
     <>
       <Header />
-      <div className="mx-24">
-        <Category />
-        <Cards />
-      </div>
+      <Category />
+      <Cards />
     </>
     //branch생성
   );
