7642ff842595dad1712e61881ec60df8fc48001a
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-05-04 00:47:42 +0900

    Header tailwind 수정, 각 요소가 왼쪽, 중간, 오른쪽 정렬 되도록 position사용, margin 수정

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ function App() {
     // 모두 구현 후 구현 완료한 페이지 캡쳐하여 pr에 완성본 항목에 사진 첨부 부탁드립니다.
     <>
       <Header />
-      <Category />
-      <Cards />
+      <div className="mx-24">
+        <Category />
+        <Cards />
+      </div>
     </>
     //branch생성
   );
