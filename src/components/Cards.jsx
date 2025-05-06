8ba3b719218203ea7f9a8a965f47ee8505bca197
Limtaehyeon <165642906+Limtaehyeon@users.noreply.github.com>
2025-05-07 03:31:02 +0900

    6회차 과제

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,90 @@
+import Category from "./Category";
+import image from "../assets/image.png";
+
+const CardList = [
+  {
+    id: 1,
+    image: image,
+    title: "게스트 선호",
+    location: "한국 Ongryong-myeon, Gwangyan...",
+    rating: 4.97,
+    distance: "134km 거리",
+    date: "6월 6일~11일",
+    comment: "따뜻하게 맞아주셔서 감사합니다",
+    price: "₩136,941 /박",
+  },
+  {
+    id: 2,
+    image: image,
+    location: "한국 Bonghwa-eup, Bonghwa-gun",
+    rating: 4.92,
+    distance: "120km 거리",
+    date: "5월 6일~11일",
+    price: "₩218,000 /박",
+  },
+  {
+    id: 3,
+    image: image,
+    location: "한국 Pungcheon-myeon, Andong",
+    rating: 4.87,
+    distance: "82km 거리",
+    date: "5월 24일~29일",
+    price: "₩176,882 / 박",
+  },
+  {
+    id: 4,
+    image: image,
+    title: "게스트 선호",
+    location: "한국 Gyo-dong, Gyeongju",
+    rating: 4.91,
+    distance: "43km 거리",
+    date: "6월 18일~23일",
+    comment: "욕실 깔끔하고 온돌 잘 됩니다",
+    price: "₩260,188 /박",
+  },
+  {
+    id: 5,
+    image: image,
+    title: "게스트 선호",
+    location: "한국 Ongryong-myeon, Gwangyan...",
+    rating: 4.97,
+    distance: "134km 거리",
+    date: "6월 6일~11일",
+    comment: "따뜻하게 맞아주셔서 감사합니다",
+    price: "₩136,941 /박",
+  },
+  {
+    id: 6,
+    image: image,
+    title: "게스트 선호",
+    location: "한국 Bonghwa-eup, Bonghwa-gun",
+    rating: 4.92,
+    distance: "120km 거리",
+    date: "5월 6일~11일",
+    price: "₩218,000 /박",
+  },
+  {
+    id: 7,
+    image: image,
+    title: "게스트 선호",
+    location: "한국 Pungcheon-myeon, Andong",
+    rating: 4.87,
+    distance: "82km 거리",
+    date: "5월 24일~29일",
+    price: "₩176,882 / 박",
+  },
+  {
+    id: 8,
+    image: image,
+    location: "한국 Gyo-dong, Gyeongju",
+    rating: 4.91,
+    distance: "43km 거리",
+    date: "6월 18일~23일",
+    comment: "욕실 깔끔하고 온돌 잘 됩니다",
+    price: "₩260,188 /박",
+  },
+];
+
 function Cards() {
   const printBox = () => {
     const boxes = Array.from({ length: 8 }, (_, i) => (
@@ -9,14 +96,52 @@ function Cards() {
   };
 
   return (
-    // 세션 때 구조 분석한 것처럼 컴포넌트 구조 분석하여(이름도 지어주세요) pr에 사진 첨부 꼭 해주세요.
-    <div>
-      {/* grid 클래스 활용해서 컴포넌트 만들기 */}
-      {/* 아래는 예시 코드입니다 */}
-      <div>Cards</div>
-      <div class="grid grid-cols-4 gap-4">
-        {printBox()}
-      </div>
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {CardList.map((card) => (
+        <div
+          id={card.id}
+          className="w-80 bg-white rounded-xl overflow-hidden transition"
+        >
+          <div className="relative">
+            <img
+              className="w-full h-80 object-cover rounded-xl"
+              src={card.image}
+              alt="숙소이미지"
+            />
+            {card.title && (
+              <div className="absolute top-2 left-2 bg-white text-black text-xs px-3 py-1 rounded-full">
+                {card.title}
+              </div>
+            )}
+            <div className="absolute top-2 right-2 text-black text-xl">♥</div>
+          </div>
+
+          <div className="text-sm mb-2 space-y-1">
+            <div className="flex justify-between items-center">
+              <div className="text-black font-semibold: 600">
+                {card.location}
+              </div>
+              <div className="text-black text-sm">★ {card.rating}</div>
+            </div>
+            <div className="mb-1">
+              <div className="text-gray-500">{card.distance}</div>
+              <div className="text-gray-500">{card.date}</div>
+              {card.comment && (
+                <div className="text-sm">
+                  <span className="text-gray-500">게스트 한마디: </span>
+                  <span className="text-black">"{card.comment}"</span>
+                </div>
+              )}
+            </div>
+            <div className="mt-2 font-semibold: 600">
+              <span className="text-black">
+                {card.price.toString().split("/")[0]}
+              </span>
+              <span className="text-gray-500"> /박</span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,38 @@
+const categoryList = [
+  { id: 1, category: "한옥" },
+  { id: 2, category: "컬처 아이콘" },
+  { id: 3, category: "해변 바로 앞" },
+  { id: 4, category: "멋진 수영장" },
+  { id: 5, category: "유서 깊은 주택" },
+  { id: 6, category: "Luxe" },
+  { id: 7, category: "한적한 시골" },
+  { id: 8, category: "최고의 전망" },
+  { id: 9, category: "기상천외한 숙소" },
+  { id: 10, category: "방" },
+  { id: 11, category: "초소형 주택" },
+  { id: 12, category: "호수 근처" },
+  { id: 13, category: "키즈" },
+];
+
 function Category() {
   return (
     // 아이콘은 신경쓰지 않고 카테고리명만 사용해서 구현해주세요.
     // 아이콘도 사용하고 싶으시면 모든 코드 완성 후 마지막에 시간 남을 때 추가하시는 걸 추천드립니다.
-    <div>
-      <div>Category</div>
-      <div>test</div>
+    <div className="flex h-20 justify-center gap-4 px-4 py-6 border-b border-gray-300 mb-4">
+      <div className="flex items-center space-x-8 text-gray-500">
+        {categoryList.map((category) => (
+          <div key={category.id}>{category.category}</div>
+        ))}
+        <div className="flex space-x-2">
+          <button className="w-8 h-8 m-2 flex items-center justify-center border border-gray-300 rounded-full text-gray-600">
+            &gt;
+          </button>
+
+          <button className="h-8 flex items-center border border-gray-300 rounded-xl px-6 py-6 text-gray-600">
+            필터
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
