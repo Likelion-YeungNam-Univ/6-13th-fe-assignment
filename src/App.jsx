ffd887e6c1831f410444d77c56f02784739ed9ea
chanju <ckswn1244@gmail.com>
2025-05-06 21:06:57 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import Cards from "./components/Cards";
-import Category from "./components/Category";
-import Header from "./components/Header";
+import Cards from './components/Cards';
+import Category from './components/Category';
+import Header from './components/Header';
+
+
 
 function App() {
-  return (
-    // 모두 구현 후 구현 완료한 페이지 캡쳐하여 pr에 완성본 항목에 사진 첨부 부탁드립니다.
-    <>
-      <Header/>
-      <Category/>
-      <Cards/>
-    </>
-  );
+  return <><Header></Header><Category></Category><Cards></Cards></>;
 }
+
 
 export default App;
