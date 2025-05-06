4efe074427b4f6bbadc9e408631c2baed79314e5
MinJeong <mineun0517@naver.com>
2025-05-06 22:13:00 +0900

    card

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   return (
     // 모두 구현 후 구현 완료한 페이지 캡쳐하여 pr에 완성본 항목에 사진 첨부 부탁드립니다.
     <>
-      <Header/>
-      <Category/>
-      <Cards/>
+      <Header />
+      <Category />
+      <Cards />
     </>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,9 @@
+import photo from "../assets/photo.jpeg";
+
 function Cards() {
-  const printBox = () => {
+  const Card = () => {
     const boxes = Array.from({ length: 8 }, (_, i) => (
-      <div key={i} className="bg-blue-400">
+      <div key={i} className="bg-blue-400 h-96">
         0{i + 1}
       </div>
     ));
@@ -9,15 +11,29 @@ function Cards() {
   };
 
   return (
-    // 세션 때 구조 분석한 것처럼 컴포넌트 구조 분석하여(이름도 지어주세요) pr에 사진 첨부 꼭 해주세요.
-    <div>
-      {/* grid 클래스 활용해서 컴포넌트 만들기 */}
-      {/* 아래는 예시 코드입니다 */}
-      <div>Cards</div>
-      <div class="grid grid-cols-4 gap-4">
-        {printBox()}
+    <>
+      // 세션 때 구조 분석한 것처럼 컴포넌트 구조 분석하여(이름도 지어주세요)
+      pr에 사진 첨부 꼭 해주세요.
+      <div>
+        <div className="grid grid-cols-4 gap-4">{Card()}</div>
+        {/* main */}
+        <div>
+          <img src={photo} />
+          <div>Satisfaction</div>
+        </div>
+        <div>
+          {/*중간*/}
+          <div>
+            <div>Title</div>
+            <div>Information</div>
+            <div>Comment</div>
+            <div>Money</div>
+          </div>
+          {/*오른쪽*/}
+          <div>Point</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
