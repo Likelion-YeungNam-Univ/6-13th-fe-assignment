d9bb8f76a5f8fd75ed1b65414176a833cf141bb8
Jiwon Park <parkjiwon0821@naver.com>
2025-05-07 00:47:31 +0900

    design: 카테고리 컴포넌트 구현

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,12 +1,63 @@
+import {
+	Bed,
+	Building2Icon,
+	Castle,
+	CookingPotIcon,
+	Filter,
+	Home,
+	Tent,
+	Ticket,
+	ToyBrick,
+	TreePalm,
+	Trees,
+	View,
+	WalletCardsIcon,
+	Waves,
+} from "lucide-react";
+
 function Category() {
-  return (
-    // 아이콘은 신경쓰지 않고 카테고리명만 사용해서 구현해주세요.
-    // 아이콘도 사용하고 싶으시면 모든 코드 완성 후 마지막에 시간 남을 때 추가하시는 걸 추천드립니다.
-    <div>
-      <div>Category</div>
-      <div>test</div>
-    </div>
-  );
+	// 카테고리 목록 리스트
+	const categories = [
+		{ id: "hanok", name: "한옥", icon: Home },
+		{ id: "luxury", name: "컬처 아이콘", icon: Ticket },
+		{ id: "beach", name: "해변 바로 앞", icon: Waves },
+		{ id: "countryside", name: "멋진 수영장", icon: TreePalm },
+		{ id: "historical", name: "유서 깊은 주택", icon: Castle },
+		{ id: "luxe", name: "Luxe", icon: CookingPotIcon },
+		{ id: "traditional", name: "한적한 시골", icon: Trees },
+		{ id: "unique", name: "최고의 전망", icon: View },
+		{ id: "camping", name: "기상천외한 숙소", icon: Tent },
+		{ id: "rooms", name: "방", icon: Bed },
+		{ id: "tropical", name: "초소형 주택", icon: Building2Icon },
+		{ id: "skiing", name: "호수 근처", icon: WalletCardsIcon },
+		{ id: "boats", name: "키즈", icon: ToyBrick },
+	];
+
+	return (
+		<div className="flex justify-center w-full border-b py-4 gap-6">
+			{/* 카테고리 버튼들 추가 */}
+			{categories.map(({ id, name, icon: Icon }) => (
+				<button
+					key={id}
+					className="flex flex-col items-center w-[100px] gap-2 text-gray-500 hover:text-black hover:border-b-2 hover:border-black"
+				>
+					<Icon className="w-6 h-6" />
+					<span className="text-xs">{name}</span>
+				</button>
+			))}
+
+			{/* '>' 버튼 */}
+			<button className="flex justify-center border rounded-full w-[50px]">
+				<span className="text-3xl">&gt;</span>
+			</button>
+
+			{/* 필터링 버튼 */}
+			<button className="flex justify-center items-center border rounded-xl w-[100px]">
+				<Filter />
+				&nbsp;필터
+			</button>
+		</div>
+	);
 }
 
 export default Category;
