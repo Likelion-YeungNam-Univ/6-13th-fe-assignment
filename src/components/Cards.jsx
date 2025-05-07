007f7ae97cdb9b105bf8bebc38707b00963c8ffe
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-07 14:09:33 +0900

    style: Cards 컴포넌트 스타일 적용

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,22 +3,30 @@ import card from "../assets/cardImg.png";
 function Cards() {
   const printBox = () => {
     const boxes = Array.from({ length: 8 }, (_, i) => (
-      <div key={i} className="">
+      <div key={i}>
         {/* 숙소 이미지 */}
-        <img src={card} />
+        <img src={card} className="w-full h-[270px] rounded-2xl" />
+
         {/* Bottom */}
-        <div>
-          <span>한국-Korea....</span>
-          <span>★4.99</span>
-          <div>134km 거리</div>
-          <div>5월 5일~6일</div>
-          <div>
-            <span>게스트 한마디</span>
-            <span> "멋져요ㅎvㅎ"</span>
+        <div className="mt-2">
+          {/* 장소, 평점 */}
+          <div className="flex justify-between">
+            <span>한국 Gyo-dong, Daegu</span>
+            <span>★4.99</span>
           </div>
+          {/* 거리 */}
+          <div className="text-sm text-gray-600">134km 거리</div>
+          {/* 날짜 */}
+          <div className="text-sm text-gray-600">5월 5일~6일</div>
+          {/* 게스트 한마디 */}
           <div>
-            <span>\136,941</span>
-            <span> /박</span>
+            <span className="text-sm text-gray-600">게스트 한마디</span>
+            <span className="text-sm"> "멋져요ㅎvㅎ"</span>
+          </div>
+          {/* 가격 */}
+          <div>
+            <span className="text-sm font-bold">\136,941</span>
+            <span className="text-sm"> /박</span>
           </div>
         </div>
       </div>
@@ -28,7 +36,9 @@ function Cards() {
 
   return (
     <div>
-      <div class="grid grid-cols-4 gap-4">{printBox()}</div>
+      <div className="grid grid-cols-4 gap-7 mt-5 ml-24 mr-24">
+        {printBox()}
+      </div>
     </div>
   );
 }
