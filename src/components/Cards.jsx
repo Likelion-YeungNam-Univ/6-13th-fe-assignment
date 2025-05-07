3cc007cd3ae6db04a51744aecabcf6fdf7d54984
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-07 10:04:43 +0900

    feat: Cards 컴포넌트 구조 구현

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,22 +1,34 @@
+import card from "../assets/cardImg.png";
+
 function Cards() {
   const printBox = () => {
     const boxes = Array.from({ length: 8 }, (_, i) => (
-      <div key={i} className="bg-blue-400">
-        0{i + 1}
+      <div key={i} className="">
+        {/* 숙소 이미지 */}
+        <img src={card} />
+        {/* Bottom */}
+        <div>
+          <span>한국-Korea....</span>
+          <span>★4.99</span>
+          <div>134km 거리</div>
+          <div>5월 5일~6일</div>
+          <div>
+            <span>게스트 한마디</span>
+            <span> "멋져요ㅎvㅎ"</span>
+          </div>
+          <div>
+            <span>\136,941</span>
+            <span> /박</span>
+          </div>
+        </div>
       </div>
     ));
     return boxes;
   };
 
   return (
-    // 세션 때 구조 분석한 것처럼 컴포넌트 구조 분석하여(이름도 지어주세요) pr에 사진 첨부 꼭 해주세요.
     <div>
-      {/* grid 클래스 활용해서 컴포넌트 만들기 */}
-      {/* 아래는 예시 코드입니다 */}
-      <div>Cards</div>
-      <div class="grid grid-cols-4 gap-4">
-        {printBox()}
-      </div>
+      <div class="grid grid-cols-4 gap-4">{printBox()}</div>
     </div>
   );
 }
