3406933a4c484250c801a8a22a6e2ef760deb0bc
Slity <0911kjw@naver.com>
2025-05-08 03:11:55 +0900

    Card 구현완료

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,66 @@
+import pic1 from "../assets/pic1.jpg";
+import pic2 from "../assets/pic2.jpg";
+import pic3 from "../assets/pic3.jpg";
+import pic4 from "../assets/pic4.jpg";
+import pic5 from "../assets/pic5.jpg";
+import pic6 from "../assets/pic6.jpg";
+import pic7 from "../assets/pic7.jpg";
+import pic8 from "../assets/pic8.jpg";
+
+const cardData = [
+  { pic: pic1, name: "미국, 워싱턴, George Town", rate: 4.92, distance: "120km 거리", date: "5월 6일~11일", price: "₩218,000", tag: "게스트 선호", comment: "집들이 너무 이뻐요!"},
+  { pic: pic2, name: "제주도, 서귀포시, 남원읍", rate: 4.5, distance: "3km 거리", date: "5월 15일~17일", price: "₩150,000"},
+  { pic: pic3, name: "미국, SanDiego, La Jolla", rate: 4.73, distance: "1.5km 거리", date: "5월 20일~22일", price: "₩310,000", comment: "경치가 좋았어요!"},
+  { pic: pic4, name: "제주도, 서귀포시, 성산읍", rate: 4.42, distance: "2.5km 거리", date: "5월 25일~27일", price: "₩130,000", tag: "게스트 선호"},
+  { pic: pic5, name: "강원도, 속초", rate: 4.3, distance: "12.5km 거리", date: "5월 25일~27일", price: "₩180,000", tag: "게스트 선호", comment: "근처에 갈 곳이 많아요!"},
+  { pic: pic6, name: "미국, SanDiego, La Jolla", rate: 4.68, distance: "24.5km 거리", date: "6월 25일~27일", price: "₩330,000", tag: "게스트 선호"},
+  { pic: pic7, name: "전라남도, 여수", rate: 4.35, distance: "22.5km 거리", date: "7월 25일~27일", price: "₩240,000", tag: "게스트 선호"},
+  { pic: pic8, name: "강원도, 강릉", rate: 4.4, distance: "46.5km 거리", date: "8월 25일~27일", price: "₩150,000", comment: "바다랑 가까워서 좋았어요!"}
+];
+
 function Cards() {
   const printBox = () => {
-    const boxes = Array.from({ length: 8 }, (_, i) => (
-      <div key={i} className="bg-blue-400">
-        0{i + 1}
+    return cardData.map((card, i) => (
+      <div key={i} className="bg-white rounded-xl overflow-hidden space-y-2">
+        {/* 카드 사진 */}
+        <div className="relative">
+          <img src={card.pic} alt={card.name} className="w-full h-60 object-cover rounded-xl" />
+
+          {card.tag && (
+            <div className="absolute top-2 left-2 bg-white text-xs font-semibold px-2 py-1 rounded-2xl">
+              {card.tag}
+            </div>
+          )}
+
+          <button className="absolute top-2 right-2 p-1 rounded-full">
+          ♡
+          </button>
+        </div>
+
+        {/* Info */}
+        <div className="px-2 space-y-0.5">
+          <div className="flex justify-between text-sm font-semibold">
+            <span className="text-black">{card.name}</span>
+            <span>★ {card.rate}</span>
+          </div>
+          <div className="text-sm text-gray-500">{card.distance}</div>
+          <div className="text-sm text-gray-500">{card.date}</div>
+        
+          {/* 한마디 있을 경우 */}
+          {card.comment && (
+            <div className="text-sm text-gray-600">
+            게스트 한마디: "<span className="font-bold">{card.comment}</span>"
+            </div>
+          )}
+          <div className="text-sm text-black">
+            <span className="font-bold">{card.price}</span>
+            <span className="font-normal"> /박</span>
+          </div>
+        </div>
+
+
       </div>
     ));
-    return boxes;
   };
 
   return (
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,32 @@
+const categoryList = [
+  {id:1, category: "한옥"},
+  {id:2, category: "컬처 아이콘"},
+  {id:3, category: "해변 바로 앞"},
+  {id:4, category: "멋진 수영장"},
+  {id:5, category: "유서 깊은 주택"},
+  {id:6, category: "Luxe"},
+  {id:7, category: "한적한 시골"},
+  {id:8, category: "최고의 전망"},
+  {id:9, category: "기상천외한 숙소"},
+  {id:10, category: "방"},
+  {id:11, category: "초소형 주택"},
+  {id:12, category: "호수 근처"},
+  {id:13, category: "키즈"},
+  {id:14, category: ">"},
+  {id:15, category: "필터"}
+]
+
+
+
 function Category() {
   return (
     // 아이콘은 신경쓰지 않고 카테고리명만 사용해서 구현해주세요.
     // 아이콘도 사용하고 싶으시면 모든 코드 완성 후 마지막에 시간 남을 때 추가하시는 걸 추천드립니다.
     <div>
-      <div>Category</div>
-      <div>test</div>
-    </div>
+    {categoryList.map(category => (
+        <div id={category.id}>{category.category}</div>
+    ))}
+  </div>
   );
 }
 
