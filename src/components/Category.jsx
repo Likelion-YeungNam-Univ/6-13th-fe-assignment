e0c8084e620e940210660ed89efac5231356e24d
chominseo <singer100@yu.ac.kr>
2025-05-08 00:30:58 +0900

    >, 필터 button 추가

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -18,10 +18,16 @@ function Category() {
   return (
     // 아이콘은 신경쓰지 않고 카테고리명만 사용해서 구현해주세요.
     // 아이콘도 사용하고 싶으시면 모든 코드 완성 후 마지막에 시간 남을 때 추가하시는 걸 추천드립니다.
-    <div className="flex justify-between m-2 p-2 border-b border-gray-200 max-h-24">
+    <div className="flex justify-between m-1 p-1 border-b border-gray-200 max-h-24">
       {categoryList.map(category => (
-        <div id = {category.id} className ="text-sm mg-8 p-8">{category.category}</div>
+        <div id = {category.id} className ="text-sm mg-6 p-6">{category.category}</div>
       ))}
+      <button className="m-2 p-2 border-b border-gray-200 max-h-24">
+      &gt;
+   </button>
+   <button className="m-2 p-2 border-b border-gray-200 max-h-24">
+      필터
+   </button>
     </div>
   );
 }
